35af7583f06f715644564481e0e91c17c4649ea6
Agustin Sampayo <agustines82@gmail.com>
2022-11-08 21:36:34 -0300

    creo estructura del proyecto: carpetas y archivos principales

---
+++ src/components/routes/RutasAdmin.jsx
@@ -0,0 +1,5 @@
+const RutasAdmin = () => {
+    return <></>;
+};
+
+export default RutasAdmin;
---
+++ src/components/routes/RutasProtegidas.jsx
@@ -0,0 +1,5 @@
+const RutasProtegidas = () => {
+    return <></>;
+};
+
+export default RutasProtegidas;

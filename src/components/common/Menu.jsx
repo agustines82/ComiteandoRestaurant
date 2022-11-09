4031c0e75a74f0b6f8bea340e882c4f14f1f4641
Agustin Sampayo <agustines82@gmail.com>
2022-11-09 11:57:36 -0300

    estructura basica del menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,8 +1,24 @@
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 const Menu = () => {
     return (
-        <div>
-            <h1>menu</h1>
-        </div>
+        <header>
+            <Navbar bg="light" variant="light" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link}>Comiteando</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink className={"nav-item nav-link"}>Inicio</NavLink>
+                            <NavLink className={"nav-item nav-link"}>Haz tu Pedido</NavLink>
+                            <NavLink className={"nav-item nav-link"}>Aministrador</NavLink>
+                            <NavLink className={"nav-item nav-link"}>Registro</NavLink>
+                            <NavLink className={"nav-item nav-link"}>Login/out</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 };
 

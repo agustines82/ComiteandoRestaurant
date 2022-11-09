5d505011a2eb12d2d83e26e3488e70602e1cc3cd
Agustin Sampayo <agustines82@gmail.com>
2022-11-09 11:32:44 -0300

    agrego componente REgistro en la estructura y creo las rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdmin from "./components/routes/RutasAdmin";
+import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/views/Inicio";
+import HazTuPedido from "./components/views/HazTuPedido";
+import Error404 from "./components/views/Error404";
+import Login from "./components/views/Login";
+import DetalleMenu from "./components/views/menu/DetalleMenu";
 
 function App() {
-    return <div></div>;
+    return (
+        //administrar las rutas;
+        <BrowserRouter>
+            <Menu />
+            <Routes>
+                {/* dominio + path */}
+                <Route exact path="/" element={<Inicio />} />
+                <Route exact path="/haz" element={<HazTuPedido />} />
+                <Route exact path="/administrar/detalle/:id" element={<DetalleMenu />} />
+                <Route exact path="/usuario/login" element={<Login />} />
+                <Route
+                    path="/administrar/*"
+                    element={
+                        <RutasProtegidas>
+                            <RutasAdmin></RutasAdmin>
+                        </RutasProtegidas>
+                    }
+                >
+                    {" "}
+                </Route>
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/routes/RutasAdmin.jsx
+++ src/components/routes/RutasAdmin.jsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Administrador from "../views/Administrador";
+import CrearMenu from "../views/menu/CrearMenu";
+import EditarMenu from "../views/menu/EditarMenu";
+import EditarUsuario from "../views/usuario/EditarUsuario";
+import Registro from "../views/Registro";
 const RutasAdmin = () => {
-    return <></>;
+    return (
+        <>
+            <Routes>
+                <Route exact path="/" element={<Administrador />} />
+                <Route exact path="/crear" element={<CrearMenu />} />
+                <Route exact path="/editar/:id" element={<EditarMenu />} />
+                <Route exact path="/usuario/registro" element={<Registro />} />
+                <Route exact path="/editar/usuario/:id" element={<EditarUsuario />} />
+            </Routes>
+        </>
+    );
 };
 
 export default RutasAdmin;

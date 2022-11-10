61228bb268bf9132e7f2e67ea9167a97ee6d503a
Agustin Sampayo <agustines82@gmail.com>
2022-11-10 10:18:01 -0300

    agrego estilos css a los botones del navbar - el hover no me està funcionando

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -5,27 +5,31 @@ import comitiandoLogo from "../../assets/img/comitiandoLogo.jpg";
 const Menu = () => {
     return (
         <header>
-            <Navbar bg="light" variant="light" expand="lg">
-                <Container>
+            <Navbar className="backgroundGeneral" variant="light" expand="lg">
+                <Container className="mt-1">
                     <Navbar.Brand as={Link} end to="/">
                         <img src={comitiandoLogo} />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <NavLink end to="/" className={"nav-item nav-link"}>
+                            <NavLink end to="/" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
                                 Inicio
                             </NavLink>
-                            <NavLink end to="/haz" className={"nav-item nav-link"}>
+                            <NavLink
+                                end
+                                to="/haz"
+                                className={"nav-item nav-link backgroundBotones rounded fw-bolder fontTitulos fs-3 hazTuPedidoMenu"}
+                            >
                                 Haz tu Pedido
                             </NavLink>
-                            <NavLink end to="/administrar" className={"nav-item nav-link"}>
+                            <NavLink end to="/administrar" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
                                 Aministrador
                             </NavLink>
-                            <NavLink end to="/administrar/usuario/registro" className={"nav-item nav-link"}>
+                            <NavLink end to="/administrar/usuario/registro" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
                                 Registro
                             </NavLink>
-                            <NavLink end to="/usuario/login" className={"nav-item nav-link"}>
+                            <NavLink end to="/usuario/login" className={"nav-item nav-link fw-bold backgroundBotones text-white fs-5 rounded h-25"}>
                                 Login/out
                             </NavLink>
                         </Nav>

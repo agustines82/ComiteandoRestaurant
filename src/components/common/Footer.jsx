a6f61cc358abab5777f6c3f20343167cb98193fe
Agustin Sampayo <agustines82@gmail.com>
2022-11-10 12:22:30 -0300

    modifico el sistema de grillas en el footer para que sea responsive, modifico margenes. footer listo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -8,20 +8,20 @@ const Footer = () => {
                     <img src={logoFooter} alt="logo footer" />
                 </Col>
             </Row>
-            <Row className="my-5">
-                <Col>
-                    <aside>
+            <Row className="my-5 justify-content-center">
+                <Col sm={12} md={5} lg={4}>
+                    <aside className="text-center mt-3">
                         <h3>Ubicaciòn</h3>
                         <p>
                             {" "}
-                            Rolling Mall local 1,
-                            <br /> Avenida Peron 1425
+                            Rolling Mall Local 1,
+                            <br /> Avenida Peròn 1425
                             <br /> Yerba Buena, Tucumán
                         </p>
                     </aside>
                 </Col>
-                <Col>
-                    <aside>
+                <Col sm={12} md={5} lg={4}>
+                    <aside className="text-center mt-3">
                         <h3>Horarios de Atenciòn</h3>
                         <ul>
                             <li>Lun - Vie: 10 AM - 11 PM</li>
@@ -29,13 +29,13 @@ const Footer = () => {
                         </ul>
                     </aside>
                 </Col>
-                <Col>
-                    <aside>
+                <Col sm={12} md={5} lg={4}>
+                    <aside className="text-center mt-3">
                         <h3>Reservas</h3>
                         <p>E-mail: commit@comitiando.com</p>
                         <p>Tel: 0381 4-253574</p>
                         <p>
-                            <i className="bi bi-whatsapp"></i>: 381-6592001
+                            <i className="bi bi-whatsapp text-success"></i>: 381-6592001
                         </p>
                     </aside>
                 </Col>

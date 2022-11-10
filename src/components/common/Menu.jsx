2e1ea092fa2c465e13721bdd622cb6f6cd85fe3d
Agustin Sampayo <agustines82@gmail.com>
2022-11-10 10:42:51 -0300

    elimino clase nav-link de bootstrap para que funcionen los hover y modifico estilos en app.css - faltan media queries para dispositivos sm

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -5,7 +5,7 @@ import comitiandoLogo from "../../assets/img/comitiandoLogo.jpg";
 const Menu = () => {
     return (
         <header>
-            <Navbar className="backgroundGeneral" variant="light" expand="lg">
+            <Navbar className="backgroundGeneral" expand="lg">
                 <Container className="mt-1">
                     <Navbar.Brand as={Link} end to="/">
                         <img src={comitiandoLogo} />
@@ -13,23 +13,25 @@ const Menu = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <NavLink end to="/" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
+                            <NavLink end to="/" className={"nav-item linksMenu fw-bold fontTitulos fs-3 hoverLinksMenu"}>
                                 Inicio
                             </NavLink>
                             <NavLink
                                 end
                                 to="/haz"
-                                className={"nav-item nav-link backgroundBotones rounded fw-bolder fontTitulos fs-3 hazTuPedidoMenu"}
+                                className={
+                                    "nav-item linksMenu backgroundBotones rounded fw-bolder fontTitulos fs-3 hazTuPedidoMenu hoverHazTuPedidoMenu"
+                                }
                             >
                                 Haz tu Pedido
                             </NavLink>
-                            <NavLink end to="/administrar" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
+                            <NavLink end to="/administrar" className={"nav-item linksMenu fw-bold fontTitulos fs-3 hoverLinksMenu"}>
                                 Aministrador
                             </NavLink>
-                            <NavLink end to="/administrar/usuario/registro" className={"nav-item nav-link fw-bold fontTitulos fs-3 hoverMenu1"}>
+                            <NavLink end to="/administrar/usuario/registro" className={"nav-item linksMenu fw-bold fontTitulos fs-3 hoverLinksMenu"}>
                                 Registro
                             </NavLink>
-                            <NavLink end to="/usuario/login" className={"nav-item nav-link fw-bold backgroundBotones text-white fs-5 rounded h-25"}>
+                            <NavLink end to="/usuario/login" className={"nav-item linksMenu fw-bold backgroundBotones text-white fs-5 rounded h-25"}>
                                 Login/out
                             </NavLink>
                         </Nav>

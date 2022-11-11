bd90ce584a05397c30bb96c50a4359d4f1360de8
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-11-11 17:34:55 -0300

    realice maquetado de registrar

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -1,9 +1,61 @@
-const Registro = () => {
-    return (
-        <div>
-            <h1>registro</h1>
-        </div>
-    );
-};
+import React, { useState } from 'react'
+import { Button, Form, Modal } from 'react-bootstrap'
 
-export default Registro;
+const Registro = () => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal className='backgroundBotones' show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className='text-center fontTitulos'>Registrarse</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className='letra'>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Eliseo"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="*********"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className='justify-content-center'>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="dark" onClick={handleClose}>
+            Registrarse
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default Registro

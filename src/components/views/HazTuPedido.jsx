f6681fae1a511f02d132e2deb2dfd7fb55a0337f
Lucia Gonzalez Vidalon <luciabegeve@gmail.com>
2022-11-11 16:19:00 -0300

    agregue iconos

--- src/components/views/HazTuPedido.jsx
+++ src/components/views/HazTuPedido.jsx
@@ -10,9 +10,9 @@ const HazTuPedido = () => {
       <Card className="CardMiPedido">
         <Card.Title className="text-center fs-4 mt-3">Mi pedido</Card.Title>
         <hr />
-        <ListGroup variant="flush">
-          <ListGroup.Item className="backgroundBotones">Item 1</ListGroup.Item>
-          <ListGroup.Item className="backgroundBotones">Item 2</ListGroup.Item>
+        <ListGroup variant="flush" className="itemspedido">
+          <ListGroup.Item className=""><i class="bi bi-plus-circle-fill"></i><i class="bi bi-trash3-fill " ></i></ListGroup.Item>
+          <ListGroup.Item className="backgroundBotones"><i class="bi bi-plus-circle-fill"></i> Item 2 <i class="bi bi-trash3-fill "></i></ListGroup.Item>
         </ListGroup>
         <Card.Text className="mt-4 m-3">Total:</Card.Text>
         <div className="text-center">

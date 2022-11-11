cea3d2e4eedee09c5d3b8bd2f932a7fa1f0ddc28
Lucia Gonzalez Vidalon <luciabegeve@gmail.com>
2022-11-11 12:44:56 -0300

    estructura de mipedido

--- src/components/views/HazTuPedido.jsx
+++ src/components/views/HazTuPedido.jsx
@@ -1,9 +1,29 @@
+import { Card, ListGroup, Button } from "react-bootstrap";
+
 const HazTuPedido = () => {
-    return (
-        <div>
-            <h1>haz tu pedido</h1>
+  return (
+    <>
+      <div>
+        <h1>Haz tu pedido</h1>
+      </div>
+      <div className="PosicionMipedido">
+      <Card className="CardMiPedido">
+        <Card.Title className="text-center fs-4 mt-3">Mi pedido</Card.Title>
+        <hr />
+        <ListGroup variant="flush">
+          <ListGroup.Item className=" ">Item 1</ListGroup.Item>
+          <ListGroup.Item className=" ">Item 2</ListGroup.Item>
+        </ListGroup>
+        <Card.Text className="mt-4 m-3">Total:</Card.Text>
+        <div className="text-center">
+          <Button className="my-3 p-3 w-50 backgroundBotones">
+            Confirmar pedido
+          </Button>
         </div>
-    );
+      </Card>
+      </div>
+    </>
+  );
 };
 
 export default HazTuPedido;

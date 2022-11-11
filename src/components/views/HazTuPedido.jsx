d7ebc34993c1d3e5c9a1c9a07aa802e6f4952f09
Lucia Gonzalez Vidalon <luciabegeve@gmail.com>
2022-11-11 13:17:50 -0300

    agregue backgrounds a botones

--- src/components/views/HazTuPedido.jsx
+++ src/components/views/HazTuPedido.jsx
@@ -11,12 +11,12 @@ const HazTuPedido = () => {
         <Card.Title className="text-center fs-4 mt-3">Mi pedido</Card.Title>
         <hr />
         <ListGroup variant="flush">
-          <ListGroup.Item className=" ">Item 1</ListGroup.Item>
-          <ListGroup.Item className=" ">Item 2</ListGroup.Item>
+          <ListGroup.Item className="backgroundBotones">Item 1</ListGroup.Item>
+          <ListGroup.Item className="backgroundBotones">Item 2</ListGroup.Item>
         </ListGroup>
         <Card.Text className="mt-4 m-3">Total:</Card.Text>
         <div className="text-center">
-          <Button className="my-3 p-3 w-50 backgroundBotones">
+          <Button variant='danger'className="my-3 p-3 w-50 backgroundBotones">
             Confirmar pedido
           </Button>
         </div>

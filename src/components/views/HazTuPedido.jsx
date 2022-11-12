30ec314c75f8b9c141ab43bffeef616d7d8a1fe7
Lucia Gonzalez Vidalon <luciabegeve@gmail.com>
2022-11-12 19:04:45 -0300

    agregue card de texto subtotal

--- src/components/views/HazTuPedido.jsx
+++ src/components/views/HazTuPedido.jsx
@@ -13,7 +13,9 @@ const HazTuPedido = () => {
         <ListGroup variant="flush">
           <ListGroup.Item className="d-flex my-2 justify-content-between"> <span className="botonpedido"><i class="bi bi-plus-circle-fill"></i></span> <span className="botonpedido2"> <i class="bi bi-trash3-fill " ></i></span></ListGroup.Item>
         </ListGroup>
-        <Card.Text className="mt-4 m-3">Total:</Card.Text>
+        <Card.Text className="mt-4 m-3">Subtotal:</Card.Text>
+        <Card.Text className="m-3">Total:</Card.Text>
+
           <div className="d-flex justify-content-center">
           <button className="my-3 p-3 botonconf">
             Confirmar pedido

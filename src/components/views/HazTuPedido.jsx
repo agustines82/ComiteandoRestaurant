853c022280cc68f89b9716b4547d837cb7318dc8
Facundo Montiel <montiel.facu@gmail.com>
2022-11-13 22:28:25 -0300

    Maquetado del la card de menu y pagina de pedidos con productos

--- src/components/views/HazTuPedido.jsx
+++ src/components/views/HazTuPedido.jsx
@@ -1,8 +1,24 @@
+import { Row, Container, Col } from "react-bootstrap";
+import CardMenu from "./menu/CardMenu";
+
 const HazTuPedido = () => {
     return (
-        <div>
-            <h1>haz tu pedido</h1>
-        </div>
+        <Container>
+            <h1 className="text-center my-4">Haz tu pedido</h1>
+            <Row>
+                <Col md={8}>
+                    <Row>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    <CardMenu></CardMenu>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 

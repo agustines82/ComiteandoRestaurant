3890b9283e5df8620f7fa35e2481d755f390568c
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-11-15 19:27:11 -0300

    realice el maquetado de error 404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Link } from "react-router-dom";
 const Error404 = () => {
     return (
-        <div>
-            <h1>error 404</h1>
+        <div className="d-flex flex-column"> 
+            <h1 className="text-center fontTitulos">Error</h1>
+            <Player src="https://assets9.lottiefiles.com/packages/lf20_a2svj19k.json"  className="player errorLottie" loop controls autoplay></Player>
+            <p className="text-center">PAGINA NO ENCONTRADA</p>
+            <Link to={"/"} type='button' className='btn boton'>Volver al inicio</Link>
         </div>
     );
 };

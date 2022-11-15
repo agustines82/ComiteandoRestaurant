db85c07c357aa53ec3b43ff2f373e4925ec3078e
Agustin Sampayo <agustines82@gmail.com>
2022-11-15 10:11:52 -0300

    read ItemUsuario

--- src/components/views/usuario/ItemUsuario.jsx
+++ src/components/views/usuario/ItemUsuario.jsx
@@ -1,67 +1,18 @@
 import { Form } from "react-bootstrap";
 
-const ItemUsuario = () => {
+const ItemUsuario = ({ usuario, setUsuario }) => {
+    const { _id, nombre, email, password, perfil, estado } = { ...usuario };
     return (
         <>
             <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
+                <th className="text-truncate thLargo">{_id}</th>
+                <th>{nombre}</th>
+                <th>{email}</th>
+                <th>{password}</th>
+                <th>{perfil}</th>
                 <th className="width-1">
                     <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
                     {/* con checked podes modificar el estado del switch entre on y off */}
-                </th>
-            </tr>
-            <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
-                <th className="width-1">
-                    <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
-                </th>
-            </tr>
-            <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
-                <th className="width-1">
-                    <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
-                </th>
-            </tr>
-            <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
-                <th className="width-1">
-                    <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
-                </th>
-            </tr>
-            <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
-                <th className="width-1">
-                    <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
-                </th>
-            </tr>
-            <tr>
-                <th className="text-truncate thLargo">45dsf46asf4fd8fs45f64ssd</th>
-                <th>Agustin Sampayo</th>
-                <th>agustinsampayo@mail.com</th>
-                <th>*******</th>
-                <th>Admin / Cliente</th>
-                <th className="width-1">
-                    <Form.Check defaultChecked className="ms-3 mt-1" type="switch" id="custom-switch" />
                 </th>
             </tr>
         </>

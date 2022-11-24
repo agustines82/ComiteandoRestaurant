c53a79c55195e6fb9482303ae34faa760058e1b2
Lucia Gonzalez Vidalon <luciabegeve@gmail.com>
2022-11-24 12:47:23 -0300

    agregue imagenes y css en pagina nosotros

--- src/components/views/Nosotros.jsx
+++ src/components/views/Nosotros.jsx
@@ -1,72 +1,81 @@
-import { Card, CardImg } from "react-bootstrap";
+import { Card, CardImg, Container, Row } from "react-bootstrap";
+import Agustin from '../../assets/img/AgustinSampayo.jpg'
+import Eliseo from '../../assets/img/EliseoEspindola.jpg'
+import Facundo from '../../assets/img/FacundoMontiel.jpg'
+import Lucia from '../../assets/img/LuciaGonzalez.jpg'
+import Tomas from '../../assets/img/TomasLuna.jpg'
+import Gregorio from '../../assets/img/GregorioLlobeta.jpg'
 const Nosotros = () => {
   return (
-    <main className="container">
-      <div className="text-center">
-        <h1>Acerca de nosotros</h1>
-      </div>
-     <section className="row justify-content-center">
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
-          <CardImg 
+    <Container className="">
+      <h1 className="tittlenos">Acerca de nosotros</h1>
+   < Row className="justify-content-center">
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
+          <CardImg  className="cards"
           variant="top"
-          src=""
+          src={Agustin}
           />
-          <Card.Body>
-            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
-            <Card.Text></Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
-          <Card.Img
-            variant="top"
-            src="https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?cs=srgb&dl=pexels-abdullah-ghatasheh-1631677.jpg&fm=jpg&_gl=1*1kfk34*_ga*MTg2Mzc3MzE4NC4xNjY4MTk1MzAy*_ga_8JE65Q40S6*MTY2ODE5NTMwMy4xLjEuMTY2ODE5NjI4OS4wLjAuMA.."         />
           <Card.Body>
             <Card.Title className="text-center">Agustin Sampayo</Card.Title>
             <Card.Text></Card.Text>
           </Card.Body>
         </Card>
-
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
           <Card.Img
+          className="cards"
             variant="top"
-            src="https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?cs=srgb&dl=pexels-abdullah-ghatasheh-1631677.jpg&fm=jpg&_gl=1*1kfk34*_ga*MTg2Mzc3MzE4NC4xNjY4MTk1MzAy*_ga_8JE65Q40S6*MTY2ODE5NTMwMy4xLjEuMTY2ODE5NjI4OS4wLjAuMA.."         />
+            src= {Eliseo}       />
           <Card.Body>
-            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
-            <Card.Text></Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
-          <Card.Img
-            variant="top"
-            src="https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?cs=srgb&dl=pexels-abdullah-ghatasheh-1631677.jpg&fm=jpg&_gl=1*1kfk34*_ga*MTg2Mzc3MzE4NC4xNjY4MTk1MzAy*_ga_8JE65Q40S6*MTY2ODE5NTMwMy4xLjEuMTY2ODE5NjI4OS4wLjAuMA.."         />
-          <Card.Body>
-            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
-            <Card.Text></Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
-          <Card.Img
-            variant="top"
-            src="https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?cs=srgb&dl=pexels-abdullah-ghatasheh-1631677.jpg&fm=jpg&_gl=1*1kfk34*_ga*MTg2Mzc3MzE4NC4xNjY4MTk1MzAy*_ga_8JE65Q40S6*MTY2ODE5NTMwMy4xLjEuMTY2ODE5NjI4OS4wLjAuMA.."         />
-          <Card.Body>
-            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
-            <Card.Text></Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="col-sm-12 col-md-6 col-lg-3 m-3">
-          <Card.Img
-            variant="top"
-            src="https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?cs=srgb&dl=pexels-abdullah-ghatasheh-1631677.jpg&fm=jpg&_gl=1*1kfk34*_ga*MTg2Mzc3MzE4NC4xNjY4MTk1MzAy*_ga_8JE65Q40S6*MTY2ODE5NTMwMy4xLjEuMTY2ODE5NjI4OS4wLjAuMA.."         />
-          <Card.Body>
-            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
+            <Card.Title className="text-center">Eliseo Espindola</Card.Title>
             <Card.Text></Card.Text>
           </Card.Body>
         </Card>
 
-        </section>
-        
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
+          <Card.Img
+          className="cards"
+            variant="top"
+            src={Facundo}         />
+          <Card.Body>
+            <Card.Title className="text-center">Facundo Montiel</Card.Title>
+            <Card.Text></Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
+          <Card.Img
+          className="cards"
+            variant="top"
+            src={Lucia}         />
+          <Card.Body>
+            <Card.Title className="text-center">Lucia Gonzalez</Card.Title>
+            <Card.Text></Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
+          <Card.Img
+          className="cards"
+            variant="top"
+            src={Tomas}         />
+          <Card.Body>
+            <Card.Title className="text-center">Tomas Luna</Card.Title>
+            <Card.Text></Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="col-sm-12 col-md-6 col-lg-3 m-4 cards bgcard">
+          <Card.Img
+          className="cards"
+            variant="top"
+            src={Gregorio}         />
+          <Card.Body>
+            <Card.Title className="text-center">Gregorio Llobeta</Card.Title>
+            <Card.Text></Card.Text>
+          </Card.Body>
+        </Card>
+
+        </Row>
+        </Container>
       
-    </main>
+    
   );
 };
 

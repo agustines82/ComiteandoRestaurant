43bc6199e951714380c1044f82bb42d313868f9c
Facundo Montiel <montiel.facu@gmail.com>
2022-11-27 11:40:23 -0300

    Modifique la redireccion de la ruta protegida

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -3,7 +3,7 @@ const RutasProtegidas = ({ children }) => {
     const token = JSON.parse(localStorage.getItem("usuarioLogueado")) || null;
     if (!token) {
         //si el token es null entonces lo redirecciono al login
-        return <Navigate to={"/usuario/login"}></Navigate>;
+        return <Navigate to={"/"}></Navigate>;
     } else {
         return children;
     }

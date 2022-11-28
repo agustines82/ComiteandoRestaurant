98288fb28fabc7ae40d02c890f4f5459089d482f
Agustin Sampayo <agustines82@gmail.com>
2022-11-28 08:09:49 -0300

    corrigo pagina de error

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,14 +1,15 @@
 import { Player } from "@lottiefiles/react-lottie-player";
+import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 const Error404 = () => {
     return (
-        <div className="d-flex flex-column">
+        <Container className="d-flex flex-column">
             <Player src="https://assets9.lottiefiles.com/packages/lf20_a2svj19k.json" className="player errorLottie" loop controls autoplay></Player>
             <p className="text-center">PAGINA NO ENCONTRADA</p>
             <Link to={"/"} type="button" className="btn boton btn-sm">
                 Volver al inicio
             </Link>
-        </div>
+        </Container>
     );
 };
 

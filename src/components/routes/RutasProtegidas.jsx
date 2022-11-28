11933653dd889cd5019cd0c8cdd5beadc149e94b
Facundo Montiel <montiel.facu@gmail.com>
2022-11-28 14:24:59 -0300

    Agregue seguridad en la pagina administrador

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 const RutasProtegidas = ({ children }) => {
     const token = JSON.parse(localStorage.getItem("usuarioLogueado")) || null;
-    if (!token) {
+    if (!token || token.usuario.perfil !== "Administrador") {
         //si el token es null entonces lo redirecciono al login
         return <Navigate to={"/"}></Navigate>;
     } else {
